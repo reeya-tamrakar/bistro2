28884c688f8a028323dc7ba4617fd61a43bf7c19
reeya-tamrakar <reeya6792@gmail.com>
2020-01-01 09:52:57 +0545

    changess

--- bistro/bistro.sql
+++ bistro/bistro.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 31, 2019 at 11:40 AM
+-- Generation Time: Jan 01, 2020 at 05:05 AM
 -- Server version: 10.1.35-MariaDB
 -- PHP Version: 7.2.9
 
@@ -43,7 +43,8 @@ CREATE TABLE `admins` (
 --
 
 INSERT INTO `admins` (`username`, `password`, `id`, `email`, `phone`, `location`, `description`) VALUES
-('reeya1', '040ac25923ee3bcb2fcc276476e5b5b3', 0, 'reeya6792@gmail.com', '9818232669', 'Lalitpur', 'think positive,think high!');
+('reeya1', '040ac25923ee3bcb2fcc276476e5b5b3', 1, 'reeya6792@gmail.com', '9818232669', 'haugal', 'reeya'),
+('priscilla', '040ac25923ee3bcb2fcc276476e5b5b3', 2, 'prisci1994@gmail.com', '9231492411', 'Ekantakuna', 'i love programming.');
 
 -- --------------------------------------------------------
 
@@ -93,7 +94,10 @@ CREATE TABLE `cuisines` (
 
 INSERT INTO `cuisines` (`id`, `name`, `category`, `cat_id`, `type_of_qty`, `description`, `img1`, `img2`, `img3`, `warmth`, `type`) VALUES
 (2, 'Momo', 'Nepalese', 3, 'plate', 'Momo is in the taste of every nepalese.', '5dbb0605bfc688.95352853.jpg', '5dbb0605bfc6f7.63955183.jpeg', '5dbb0605bfc714.62290128.jpeg', 'not applicable', 'sour,veg,non-veg,spicy'),
-(3, 'Idli', 'Indian', 4, 'plate', 'Eaten by Aiyers.', '5dbb072b82bb17.65611063.jpg', '5dbb072b82bc52.02107508.jpeg', '5dbb072b82bcc8.19867735.jpeg', 'hot', 'sweet,n/a');
+(3, 'Idli', 'Indian', 4, 'plate', 'Eaten by Aiyers.', '5dbb072b82bb17.65611063.jpg', '5dbb072b82bc52.02107508.jpeg', '5dbb072b82bcc8.19867735.jpeg', 'hot', 'sweet,n/a'),
+(4, 'Chatpate', 'Nepalese', 3, 'plate', 'Chatpate is spicy authentic Nepali dish.', '5e0c12f88e20e9.67645863.jpeg', '5e0c12f88e21a7.84817501.jpeg', '5e0c12f88e21e0.68609983.jpeg', 'not applicable', 'sour,spicy'),
+(5, 'chinese food', 'Chinese', 5, 'bowl', 'a chinese food', '5e0c1399aa4b45.02400589.jpeg', '5e0c1399aa4c29.55557793.jpeg', '5e0c1399aa4c54.48292002.jpeg', 'hot', 'sweet,spicy'),
+(6, 'chinese food 2', 'Chinese', 5, 'bowl', 'chinese food 2', '5e0c152a51bff4.09382354.jpeg', '5e0c152a51c0c8.11736650.jpeg', '5e0c152a51c104.24069880.jpeg', 'hot', 'veg,non-veg');
 
 -- --------------------------------------------------------
 
@@ -118,7 +122,40 @@ INSERT INTO `customers` (`id`, `name`, `email`, `phone`, `location`, `password`)
 (21, 'rey', 'reeya6792@gmail.com', '9818232669', 'haugal', '97065a3f6bb3708842810b42214223d7'),
 (26, 'menu', 'menu@gmail.com', '1234567890', 'Australia', 'ff620737c282c43f0a1c256cf95b22da'),
 (29, 'ash_customer', 'praash47@gmail.com', '9813057360', 'Haugal, Lalitpur', 'dbc0f3a8cc39eda28a9f8ef326371f56'),
-(0, 'ads', 'adsh@adsh.com', '9822342121', 'nepal', 'e5a8d50ed67e73637b7ef2f0645720eb');
+(30, 'ads', 'adsh@adsh.com', '9822342121', 'nepal', 'e5a8d50ed67e73637b7ef2f0645720eb');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `menucats`
+--
+
+CREATE TABLE `menucats` (
+  `id` int(11) NOT NULL,
+  `catname` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `menucats`
+--
+
+INSERT INTO `menucats` (`id`, `catname`) VALUES
+(3, 'Nepalese'),
+(5, 'Chinese'),
+(4, 'Indian');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `menucuis`
+--
+
+CREATE TABLE `menucuis` (
+  `catname` varchar(255) NOT NULL,
+  `catid` int(11) NOT NULL,
+  `cuiname` varchar(255) NOT NULL,
+  `cuiid` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -160,6 +197,74 @@ CREATE TABLE `subscriptions` (
 INSERT INTO `subscriptions` (`email`) VALUES
 ('laxmishilpakar@gmail.com'),
 ('nabinm504@gmail.com');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `admins`
+--
+ALTER TABLE `admins`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `cuisinecats`
+--
+ALTER TABLE `cuisinecats`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `cuisines`
+--
+ALTER TABLE `cuisines`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `customers`
+--
+ALTER TABLE `customers`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `slides`
+--
+ALTER TABLE `slides`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `admins`
+--
+ALTER TABLE `admins`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `cuisinecats`
+--
+ALTER TABLE `cuisinecats`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `cuisines`
+--
+ALTER TABLE `cuisines`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `customers`
+--
+ALTER TABLE `customers`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=31;
+
+--
+-- AUTO_INCREMENT for table `slides`
+--
+ALTER TABLE `slides`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
